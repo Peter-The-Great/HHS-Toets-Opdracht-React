8f6489c71722525aed25375a893fb0080a508d53
Peter-The-Great <PjotrW15@Gmail.com>
2023-12-08 15:43:34 +0100

    voeg counter

--- HHS_CSharp_React/ClientApp/src/components/Counter.js
+++ HHS_CSharp_React/ClientApp/src/components/Counter.js
@@ -6,14 +6,38 @@ export class Counter extends Component {
   constructor(props) {
     super(props);
     this.state = { currentCount: 0 };
-    this.incrementCounter = this.incrementCounter.bind(this);
+      this.incrementCounter = this.incrementCounter.bind(this);
+      this.decrementCounter = this.decrementCounter.bind(this);
+      this.multibleCounter = this.multibleCounter.bind(this);
+      this.divideCounter = this.divideCounter.bind(this);
+      this.resetCounter = this.resetCounter.bind(this);
   }
 
   incrementCounter() {
     this.setState({
       currentCount: this.state.currentCount + 1
     });
-  }
+    }
+    decrementCounter() {
+    this.setState({
+      currentCount: this.state.currentCount - 1
+    });
+    }
+    multibleCounter() {
+    this.setState({
+      currentCount: this.state.currentCount * 2
+    });
+    }
+    divideCounter() {
+    this.setState({
+      currentCount: this.state.currentCount /2
+    });
+    }
+    resetCounter() {
+    this.setState({
+      currentCount: 0
+    });
+    }
 
   render() {
     return (
@@ -23,8 +47,13 @@ export class Counter extends Component {
         <p>This is a simple example of a React component.</p>
 
         <p aria-live="polite">Current count: <strong>{this.state.currentCount}</strong></p>
-
-        <button className="btn btn-primary" onClick={this.incrementCounter}>Increment</button>
+            <ul class="list-group">
+                <button className="btn btn-primary list-group-item mt-1" onClick={this.incrementCounter}>Increment</button>
+                <button className="btn btn-primary list-group-item mt-1" onClick={this.decrementCounter}>Decrement</button>
+                <button className="btn btn-primary list-group-item mt-1" onClick={this.multibleCounter}>Multiply</button>
+                <button className="btn btn-primary list-group-item mt-1" onClick={this.divideCounter}>Divide</button>
+                <button className="btn btn-primary list-group-item mt-1" onClick={this.resetCounter}>Reset</button>
+            </ul>
       </div>
     );
   }
